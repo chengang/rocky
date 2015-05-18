08b7e46c913cf7d4e1786a3d5da830236b388392
Chen Gang <chengang@cpan.org>
2015-05-18 09:55:27 +0800

    main

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+extern crate rocky;
+use rocky::{Rocky, Request, Response};
+
+fn main() {
+    fn default_handler(req: Request) -> Response {
+        let mut resp = Response::new();
+        resp.echo("404 Not Found.\n");
+        resp.echo(&req.request_script);
+        resp.echo(" lost.");
+        return resp;
+    }
+
+    fn handler(req: Request) -> Response {
+        let mut resp = Response::new();
+        resp.echo("hello world.\n");
+        resp.echo("your request_uri is:");
+        resp.echo(&req.request_script);
+        return resp;
+    }
+
+    let mut rocky = Rocky::new("127.0.0.1", 4321);
+    rocky.router.get("default", default_handler );
+    rocky.router.get("/hello_world", handler );
+    rocky.run();
+}
