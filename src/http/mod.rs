0bfce4c733747ae85a315c86de3c2e9396ffaa97
Chen Gang <chengang@cpan.org>
2015-05-31 16:43:25 +0800

    reformat

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,4 +1,4 @@
-pub mod http_helper;
+mod http_helper;
 
 use std::path::Path;
 use std::net::TcpStream;
@@ -66,12 +66,12 @@ fn get_request_line(stream: &TcpStream) -> RequestLine {
 
     RequestLine {
         method: method, 
-            request_uri: request_uri, 
-            protocol_version: protocol_version,
-            request_script: request_script,
-            request_script_ext: request_script_ext,
-            query_string: query_string,
-            get_argv: get_argv,
+        request_uri: request_uri, 
+        protocol_version: protocol_version,
+        request_script: request_script,
+        request_script_ext: request_script_ext,
+        query_string: query_string,
+        get_argv: get_argv,
     }
 }
 
@@ -119,35 +119,33 @@ pub fn handle_client(mut stream: TcpStream, router: HashMap<String, fn(Request)-
     let mut status = String::new();
     let request_info = get_request_info(&stream);
 
-    if request_info.request_script_ext.eq("css") 
-        || request_info.request_script_ext.eq("js") {
-            status.push_str("200 OK");
-            let path = Path::new(&request_info.request_uri);
-            response = file2response(path);
-    } else if request_info.request_script_ext.eq("jpg")
-        || request_info.request_script_ext.eq("png") {
-            let path = Path::new(&request_info.request_uri);
-            let binary_response = file2vec(path);
-            let response = format!("HTTP/1.0 {}\r\n\
+    if request_info.request_script_ext.eq("css") || request_info.request_script_ext.eq("js") {
+        status.push_str("200 OK");
+        let path = Path::new(&request_info.request_uri);
+        response = file2response(path);
+    } else if request_info.request_script_ext.eq("jpg") || request_info.request_script_ext.eq("png") {
+        let path = Path::new(&request_info.request_uri);
+        let binary_response = file2vec(path);
+        let response = format!("HTTP/1.0 {}\r\n\
                    Server: Rocky\r\n\
                    Content-Length: {}\r\n\
                    \r\n",
                    status, binary_response.len());
-            let _ =  stream.write(response.as_bytes());
-            let _ =  stream.write(&binary_response);
+        let _ =  stream.write(response.as_bytes());
+        let _ =  stream.write(&binary_response);
     } else {
-            if router.contains_key(&request_info.request_script) {
-                status.push_str("200 OK");
-                let handler = router.get(&request_info.request_script).unwrap();
-                response = handler(request_info);
-            } else if router.contains_key("default") {
-                status.push_str("200 OK");
-                let handler = router.get("default").unwrap();
-                response = handler(request_info);
-            } else {
-                status.push_str("404 Not Found");
-                response.body.push_str("Not Found");
-            }
+        if router.contains_key(&request_info.request_script) {
+            status.push_str("200 OK");
+            let handler = router.get(&request_info.request_script).unwrap();
+            response = handler(request_info);
+        } else if router.contains_key("default") {
+            status.push_str("200 OK");
+            let handler = router.get("default").unwrap();
+            response = handler(request_info);
+        } else {
+            status.push_str("404 Not Found");
+            response.body.push_str("Not Found");
+        }
     }
 
     let response = format!("HTTP/1.0 {}\r\n\
